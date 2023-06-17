5bee88f312dbf6e7c5ed20803a0f2f253b363431
beomsun1234 <beomsun22@gmail.com>
2023-06-18 01:58:32 +0900

    Feat: #10 token 저장, 삭제, 조회

---
+++ domain/token.go
@@ -0,0 +1,44 @@
+package domain
+
+import (
+	"strconv"
+	"time"
+)
+
+type Token struct {
+	AccessToken string
+	IssuedAt    string
+	ExpiresIn   string
+}
+
+func NewToken() *Token {
+	return &Token{}
+}
+
+func (t *Token) BuildAccessToken(accessToken string) *Token {
+	t.AccessToken = accessToken
+	return t
+}
+func (t *Token) BuildIssuedAt(issuedAt string) *Token {
+	t.IssuedAt = issuedAt
+	return t
+}
+func (t *Token) BuildExpiresIn(expiresIn string) *Token {
+	t.ExpiresIn = expiresIn
+	return t
+}
+
+func (t *Token) IsTokenExpired() bool {
+	//todo string date -> date
+	if t.AccessToken == "" || t.ExpiresIn == "" || t.IssuedAt == "" {
+		return false
+	}
+	tokenIssedAt, _ := time.Parse("2006-01-02 15:04:05", t.IssuedAt)
+	expiresIn, _ := strconv.ParseInt(t.ExpiresIn, 10, 64)
+	expiredAt := tokenIssedAt.Add(time.Second * time.Duration(expiresIn))
+	now := time.Now()
+	if expiredAt.Before(now) || expiredAt.Equal(now) {
+		return true
+	}
+	return false
+}
---
+++ repository/kis.go
@@ -0,0 +1,59 @@
+package repository
+
+import (
+	"context"
+	"encoding/json"
+	"github/beomsun1234/stockprice-collector/database"
+	"github/beomsun1234/stockprice-collector/domain"
+	"log"
+	"time"
+)
+
+type KisAccessTokenRepositoryInterface interface {
+	GetKisAccessToken() (*domain.Token, error)
+	DeleteKisAccessToken()
+	InsertKisAccessToken(token *domain.Token) error
+}
+
+type KisAccessTokenRepository struct {
+	Redis database.Database
+}
+
+func NewKisAccessTokenRepository(di_rdb database.Database) KisAccessTokenRepositoryInterface {
+	return &KisAccessTokenRepository{
+		Redis: di_rdb,
+	}
+}
+
+func (k *KisAccessTokenRepository) GetKisAccessToken() (*domain.Token, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+	data, err := k.Redis.Client().HGet(ctx, "token", "token").Result()
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	token := domain.NewToken()
+	json.Unmarshal([]byte(data), token)
+
+	return token, nil
+}
+
+func (k *KisAccessTokenRepository) DeleteKisAccessToken() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	k.Redis.Client().Del(ctx, "token")
+	defer cancel()
+}
+
+func (k *KisAccessTokenRepository) InsertKisAccessToken(token *domain.Token) error {
+	data, _ := json.Marshal(token)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	err := k.Redis.Client().HSet(ctx, "token", "token", data).Err()
+	defer cancel()
+	if err != nil {
+		log.Println("redis insert err", err)
+		return nil
+	}
+
+	return nil
+}
